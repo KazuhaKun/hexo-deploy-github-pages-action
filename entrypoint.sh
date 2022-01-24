50a0b879f2c8102a9ee4870418fbbf67fd53417e
KazuhaKun <zou123@outlook.com>
2022-01-24 15:36:03 +0800

    del all

--- entrypoint.sh
+++ entrypoint.sh
@@ -47,7 +47,7 @@ cd "${GITHUB_WORKSPACE}"
 
 echo ">>> Install NPM dependencies ..."
 npm install
-npm install hexo-theme-aurora --save
+#npm install hexo-theme-aurora --save
 
 echo ">>> Clean folder ..."
 npx hexo clean
